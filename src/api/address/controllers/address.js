f80bb8b80d79705f68b46607f832b4deaf1289bf
noproblem2311 <quanpva.dev@gmail.com>
2024-04-11 10:36:27 +0700

    change ressponse

--- src/api/address/controllers/address.js
+++ src/api/address/controllers/address.js
@@ -6,4 +6,24 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::address.address');
+module.exports = createCoreController('api::address.address', ({ strapi }) => ({
+  async getListAddressbyCustomerId(ctx) {
+    try {
+      const customerId = ctx.params.customerid; // Corrected variable name
+
+      let entity; // Declared the variable
+
+      try {
+        entity = await strapi.entityService.findMany('api::address.address', {
+          filters: { customer: customerId }, // Changed to use customerId
+        });
+      } catch (err) {
+        ctx.throw(500, err);
+      }
+
+      ctx.body = entity; // Corrected variable name
+    } catch (err) {
+      ctx.throw(500, err);
+    }
+  }
+}));
--- src/api/address/routes/address.js
+++ src/api/address/routes/address.js
@@ -6,4 +6,27 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::address.address');
+const defaultRouter = createCoreRouter('api::address.address');
+
+const customRouter = (innerRouter, extraRoutes = []) => {
+    let routes;
+    return {
+      get prefix() {
+        return innerRouter.prefix;
+      },
+      get routes() {
+        if (!routes) routes = innerRouter.routes.concat(extraRoutes);
+        return routes;
+      },
+    };
+  };
+
+  const myExtraRoutes = [
+    {
+      method: 'GET',
+      path: '/address/customer/:customerid',
+      handler: 'api::address.address.getListAddressbyCustomerId',
+    },
+  ];
+
+  module.exports = customRouter(defaultRouter, myExtraRoutes)
