688b0b21f8d1d238667a4edd43960bea3676916e
noproblem2311 <quanpva.dev@gmail.com>
2024-04-15 14:49:51 +0700

    auth UI

--- config/server.js
+++ config/server.js
@@ -2,16 +2,6 @@ module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   // url: "https://tb.metasolutions.app",
-  // url: "https://tb.metasolutions.app",
-
-  // url: "https://tb.metasolutions.app",
-  // url: "https://tb.metasolutions.app",
-
-  // url: "https://tb.metasolutions.app",
-  // url: "https://tb.metasolutions.app",
-
-  // url: "https://tb.metasolutions.app",
-
   app: {
     keys: env.array("APP_KEYS"),
   },
--- src/api/task/controllers/task.js
+++ src/api/task/controllers/task.js
@@ -1,4 +1,7 @@
 'use strict';
+
+const { sort } = require('../../../../config/middlewares');
+
 const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::task.task', ({ strapi }) => ({
@@ -12,7 +15,6 @@ module.exports = createCoreController('api::task.task', ({ strapi }) => ({
         entity = await strapi.service('api::task.task').find({
           filters: { customer: id },
           populate: ['customer', 'tasker', 'review', 'address'],
-
           sort: { createdAt: 'desc' }, // Sắp xếp theo thời gian tạo mới nhất
           pagination: { start: 0, limit: -1 }, // Lấy tất cả các bản ghi
         });
@@ -21,7 +23,6 @@ module.exports = createCoreController('api::task.task', ({ strapi }) => ({
         entity = await strapi.service('api::task.task').find({
           filters: { tasker: id },
           populate: ['customer', 'tasker', 'review', 'address'],
-
           sort: { createdAt: 'desc' },
           pagination: { start: 0, limit: -1 },
         });
@@ -34,4 +35,22 @@ module.exports = createCoreController('api::task.task', ({ strapi }) => ({
       ctx.throw(500, err);
     }
   },
+
+  async getDetailTaskByTaskId(ctx) {
+    try {
+      const { id } = ctx.params;
+      const entity = await strapi.service('api::task.task').find({ 
+        filters: { id: id },
+        populate: ['customer', 'tasker', 'review', 'address']
+      });
+
+      if (!entity) {
+        return ctx.notFound('Task not found');
+      }
+
+      ctx.body = entity;
+    } catch (err) {
+      ctx.throw(500, err);
+    }
+  }
 }));
